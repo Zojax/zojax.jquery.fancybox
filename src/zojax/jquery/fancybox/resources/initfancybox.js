497fd81785b9f31d3a014df5f9246ccd338e43db
Dmitry Suvorov <suvdim@gmail.com>
2012-12-17 13:54:21 +0400

    added jquery.fancybox

---
+++ src/zojax/jquery/fancybox/resources/initfancybox.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+	$("a.fancybox").fancybox();
+
+	$("a.holiday-card").fancybox({
+	    'padding'           : 0,
+        'autoScale'     	: false,
+        'transitionIn'		: 'none',
+		'transitionOut'		: 'none'
+	});
+
+});
