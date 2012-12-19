bb6ef760db999f06252ece1d68aa0c1c4ae2ca07
Dmitry Suvorov <suvdim@gmail.com>
2012-12-19 13:14:00 +0400

    #387: fixed default size for  holiday-card

--- src/zojax/jquery/fancybox/resources/initfancybox.js
+++ src/zojax/jquery/fancybox/resources/initfancybox.js
@@ -1,12 +1,14 @@
 $(document).ready(function() {
 
-	$("a.fancybox").fancybox();
+    $("a.fancybox").fancybox();
 
-	$("a.holiday-card").fancybox({
-	    'padding'           : 0,
-        'autoScale'     	: false,
-        'transitionIn'		: 'none',
-		'transitionOut'		: 'none'
-	});
+    $("a.holiday-card").fancybox({
+        'padding'           : 0,
+        'autoScale'         : false,
+        'transitionIn'      : 'none',
+        'transitionOut'     : 'none',
+        'width'             : 740,
+        'height'            : 499
+    });
 
 });
